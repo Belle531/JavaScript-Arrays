b89c8a1e31382095d79d3e1c7a03f9bc60495b86
Cassandra <cassandra.moore1516@gmail.com>
2025-02-19 20:16:22 -0600

    Created script.js file

---
+++ script.js
@@ -0,0 +1,30 @@
+
+    const colors = ["red", "green", "blue"];
+    console.log(colors[0]);
+    
+    colors.push('yellow');
+    console.log(colors);
+    
+    colors.pop();
+    console.log(colors); 
+    
+    colors.unshift("purple");
+    console.log(colors);
+    
+    colors.shift(); 
+    console.log(colors);
+    
+    console.log(colors.length);
+    
+    const colorString = colors.join("-"); 
+    console.log(colorString);
+    
+    colors[1] = "pink";
+    console.log(colors);
+    
+    const numbers = [10, 20, 30, 40, 50];
+    console.log(numbers[2]);
+    
+    numbers.pop();
+    numbers.unshift(5, 10);
+    console.log(numbers);
